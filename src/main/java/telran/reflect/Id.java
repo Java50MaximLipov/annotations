dc023ff2b490d585e783ad7c7a286ade9f57bef7
mlipov <maxim.lipov@gmail.com>
2023-12-05 18:10:39 +0200

    231205 adds annotations BeforeEach, Id, Index and appropriate processing

---
+++ src/main/java/telran/reflect/Id.java
@@ -0,0 +1,12 @@
+package telran.reflect;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(FIELD)
+public @interface Id {
+}
